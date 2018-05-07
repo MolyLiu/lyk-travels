f759defb961c4d7727552fa2cd2a6de4532eb31e
阿坤 <liuyingkun96@163.com>
2018-05-07 15:03:30 +0800

    20180507 构建项目完成

---
+++ src/main/java/com/example/lyktravels/LykTravelsApplication.java
@@ -0,0 +1,11 @@
+package com.example.lyktravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LykTravelsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LykTravelsApplication.class, args);
+    }
+}
